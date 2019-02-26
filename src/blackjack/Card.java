6a03b0abadbb4a4e3ae4075342c5ad1cc7e92de7
Niranjan <niranjanakolkar@gmail.com>
2019-02-25 17:12:22 -0800

    fixed issue in Player constr

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -6,35 +6,28 @@ public class Card {
     private int value;
 
     public Card(Character imagePath, int value) {
-        this.imagePath = "/cards/back"+value+imagePath+".gif"; //taking the value of the card from the file name
-        if(value==1) {
-        	
-        	this.value=11;
+        this.imagePath = "/cards/back" + value + imagePath + ".gif"; //taking the value of the card from the file name
+        if (value == 1) {
+
+            this.value = 11;
+        } else if (value > 10) {
+
+            this.value = 10;
+        } else {
+            this.value = value;
         }
-        
-        if(value>10) {
-        	
-        	this.value=10;
-        }
-        
-        this.value = value;
     }
 
     public String getImagePath() {
-
         return imagePath;
     }
 
     public int getValue() {
-    	
-    	   		
-    		return value;
-    	
+        return this.value;
     }
-    
+
     @Override
     public String toString() {
-		return "Card Path: " + getImagePath() + " Value: " + getValue();
-    	
+        return "Card Path: " + getImagePath() + " Value: " + getValue();
     }
 }
